f48941885c0948d9b9b82c1a4c23884adde8f64f
rafael <rafael.santos7520@gmail.com>
2023-06-06 11:09:08 -0300

    feat:create global styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { ChakraBaseProvider } from "../node_modules/@chakra-ui/react/dist/chakra-provider"
+import customTheme from './styles/theme';
+import "@fontsource/inter";
+import "@fontsource/lexend"; 
 
 function App() {
-
   return (
-    <main>
-   
-    </main>
+    <ChakraBaseProvider theme={customTheme}>
+    </ChakraBaseProvider>
   )
 }
 
