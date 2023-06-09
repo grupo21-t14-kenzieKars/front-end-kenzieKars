55d97c9f5f2a9d7ac3fef909f4b1100d387442e4
rafael <rafael.santos7520@gmail.com>
2023-06-09 15:22:53 -0300

    create post container

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
-
+import PosterContainer from './../components/posterContainer';
+import { mockedCarPost, mockedCarPost2 } from './../mocks/index';
+mockedCarPost2
 const RoutesMain = () =>{
     return(
         <Routes>
