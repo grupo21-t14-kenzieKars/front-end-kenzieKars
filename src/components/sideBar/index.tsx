a6495b1735f4a4a56a6c0d68dcc146ccab353bfb
Lucas Rodrigues <lcs.rs23@gmail.com>
2023-06-12 12:19:14 -0300

    create filter price/km

--- src/components/sideBar/index.tsx
+++ src/components/sideBar/index.tsx
@@ -3,9 +3,11 @@ import InputSide from "./inputSide"
 import FilterCategory from "./filterCategory"
 import { useContext } from "react"
 import { CarContext } from "../../contexts/CarsContext"
+import { useForm } from "react-hook-form"
+import { IMockedCar } from "../../interfaces/moks.interfaces"
 
 const SideBar = () => {
-    const { carList } = useContext(CarContext)
+    const { carList, filteredCarList, setFilteredCarList } = useContext(CarContext)
 
     const filters = {
         brands: [...new Set(carList.map(ele => ele.brand))],
@@ -15,9 +17,31 @@ const SideBar = () => {
         fuels: [...new Set(carList.map(ele => ele.fuelType))]
     }
 
+    const { register, handleSubmit, reset } = useForm({
+        mode: 'onBlur',
+    })
+
+    const onSubmit = (data: any) => {
+        const newListKM = filteredCarList.filter((car: IMockedCar) => car.kilometers >= data.minKm)
+        setFilteredCarList(newListKM)
+        if (data.maxKm > 0) {
+            const newListKM = filteredCarList.filter((car: IMockedCar) => car.kilometers <= data.maxKm)
+            setFilteredCarList(newListKM)
+        }
+        const newListPrice = filteredCarList.filter((car: IMockedCar) => car.price >= data.minPrice)
+        setFilteredCarList(newListPrice)
+        if (data.maxPrice > 0) {
+            const newListPrice = filteredCarList.filter((car: IMockedCar) => car.price <= data.maxPrice)
+            setFilteredCarList(newListPrice)
+        }
+    }
+    const clearFilter = () => {
+        setFilteredCarList(carList)
+        reset()
+    }
     return (
         <Flex direction={"column"} width={{ base: '100%', md: '350px' }} alignItems={"center"} gap={5} fontFamily="heading">
-            <Flex align={"start"} direction={"column"} width={"full"} gap={5}>
+            <form onBlur={handleSubmit(onSubmit)}><Flex align={"start"} direction={"column"} width={"full"} gap={5}>
                 <FilterCategory filters={filters.brands} category={'brand'}>Marca</FilterCategory>
                 <FilterCategory filters={filters.models} category={'model'}>Modelo</FilterCategory>
                 <FilterCategory filters={filters.colors} category={'color'}>Cor</FilterCategory>
@@ -26,22 +50,27 @@ const SideBar = () => {
                 <Heading color="grey.0" fontSize="heading.5" fontWeight="semibold">
                     KM
                 </Heading>
+
                 <Flex gap='26px'>
-                    <InputSide placetext="Minima"></InputSide>
-                    <InputSide placetext="Maxima"></InputSide>
+                    <InputSide placetext="Minima" id='minKM' register={register('minKM')}></InputSide>
+                    <InputSide placetext="Maxima" id='maxKM' register={register('maxKM')}></InputSide>
                 </Flex>
                 <Heading color="grey.0" fontSize="heading.5" fontWeight="semibold">
                     Preço
                 </Heading>
                 <Flex gap='26px'>
-                    <InputSide placetext="Minimo"></InputSide>
-                    <InputSide placetext="Maximo"></InputSide>
+                    <InputSide placetext="Minimo" id='minPrice' register={register('minPrice')}></InputSide>
+                    <InputSide placetext="Maximo" id='maxPrice' register={register('maxPrice')}></InputSide>
                 </Flex>
+
             </Flex>
-            <Button
-                width='90%'
-            >Limpar filtro</Button>
-        </Flex>
+                <Button
+                    marginTop='20px'
+                    width={"full"}
+                    onClick={() => clearFilter()}
+                >Limpar filtro</Button>
+            </form>
+        </Flex >
     )
 }
 
